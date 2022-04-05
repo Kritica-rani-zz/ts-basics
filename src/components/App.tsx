eb0539a38ada14deb0605d74d2c8244c3f79eb17
Kritica <kritica1997@gmail.com>
2022-04-05 17:00:00 +0530

    ts work

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar'
+import TableData from './TableData';
+import TableSearch from './TableSearch';
+const App: React.FC =()=>{
+  return (
+    <div className="App">
+     <Navbar/>
+     <TableSearch/> 
+<TableData/>
+    </div>
+  );
+}
+
+export default App;
