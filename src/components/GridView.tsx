514efc306ad350c3cddefbe0bd4050303df484ec
Kritica <kritica1997@gmail.com>
2022-04-10 22:50:34 +0530

    calender , drag and drop added

--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -1,15 +1,17 @@
 import react from 'react'
 import './component.css'
 function GridView(props: any) {
-    const tableData = props.tableData;
+  
+    const {tableData} = props
+   
 
     return (
-        <div className= 'GridView__main'>
+        <div className= 'gridView__main'>
             {tableData && tableData.map((item: any) => {
                 return (
-                    <div className='GridView__Contents'>
-                        <div className='GridView__title'>  {item.title} </div>
-                        <div className='GridView__body'> {item.body} </div>
+                    <div className='gridView__Contents' key = {props.id}>
+                        <div className='gridView__title'>  {item.title} </div>
+                        <div className='gridView__body'> {item.body} </div>
 
                     </div>
                 )
--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,14 +1,59 @@
-import './component.css'
-const TableSearch: React.FC = () => {
+import './component.css';
+import {useState} from 'react';
+import { BsFillGrid3X3GapFill } from 'react-icons/bs';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import {useDropzone} from 'react-dropzone';
+
+
+const TableSearch= (props:any) => {
+const toggle = props.toggle
+const handleToggle =props.handleToggle
+const [value, setValue] = useState<Date | null>(null);
+
+const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+const files = acceptedFiles.map((file:any) => (
+  
+  <li key={file?.path}>
+    {file?.path} - {file?.size} bytes
+  </li>
+));
     return (
-        <div className="tableSearch-main">
-            <div className="tableSearch-main__title">
+     
+        <div className="tableSearch">
+            <div className="tableSearch__title">
                 <h2>Post Table</h2>
             </div>
-            <div className="tableSearch-main__searchbar">
+            <BsFillGrid3X3GapFill onClick={(e)=>handleToggle(e)} className= 'tableSearch-icon'/>
+            <div className="tableSearch__searchbar">
                 <button className="button-refresh">Refresh</button>
                 <input type="text" placeholder="Search" />
             </div>
+
+     <section className="dropzone">
+      <div {...getRootProps({className: 'dropzone__container'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        label="Select Date"
+        value={value}
+        onChange={(newValue:any) => {
+          setValue(newValue);
+        }}
+        renderInput={(params:any) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
         </div>
     )
 }
